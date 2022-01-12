acb75fb5ed33c669d23491b59d0ea921afeabf9a
harsh1507 <harshsingh15j@gmail.com>
2022-01-13 01:16:52 +0530

    initial_commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    <h1>THIS IS APP COMPONENT</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+import Landingpage from './LandingPage/Landing_page';
+import Postview from './PostView/Postview';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+
+    <div className="Wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landingpage />}/>
+          <Route path="/postViewPage" element={<Postview />}/>
+          
+        </Routes>
+
+      </BrowserRouter>
+*/ 
