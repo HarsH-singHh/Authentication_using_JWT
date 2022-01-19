9c0e905b32303985238ee9260b8d253ab8715a15
harsh1507 <harshsingh15j@gmail.com>
2022-01-19 15:31:42 +0530

    Token_Related_Functions_completed

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,34 +1,47 @@
 import './App.css';
-
+import React, {useState} from 'react';
 
 function App() {
+
+  const[email,setEmail] = useState("");
+  const[token,setToken] = useState("");
+
+  function handleClick(){
+    let token_in_local = window.localStorage.getItem(email);
+    
+    if (email===""){
+      setToken("Token Not Found")
+    }
+    else if(token_in_local){
+      setToken(token_in_local);
+    }else{
+      let randToken = Math.random(1000000).toString(36).substr(2);
+      window.localStorage.setItem(email,randToken);
+      setToken(window.localStorage.getItem(email));
+    }
+  }
+
   return (
     <div className="App">
       
-    <h1>THIS IS APP COMPONENT</h1>
+    <h2>PRESS LOGIN TO GENERATE TOKEN</h2>
+      <p>Email :&nbsp;
+        <input type ="text" onChange={(e)=>setEmail(e.target.value)}></input>
+      </p>
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password :&nbsp; 
+        <input type ="password"></input>
+      </p>
+      <button onClick={()=>handleClick()} >LOGIN</button>
+      <p>
+        
+        {email && <label>Token : &nbsp;</label>}
+        
+        <label>{token}</label>
+ 
+      </p>
 
     </div>
   );
 }
 
 export default App;
-
-
-/*
-import Landingpage from './LandingPage/Landing_page';
-import Postview from './PostView/Postview';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-function App() {
-  return (
-
-    <div className="Wrapper">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingpage />}/>
-          <Route path="/postViewPage" element={<Postview />}/>
-          
-        </Routes>
-
-      </BrowserRouter>
-*/ 
